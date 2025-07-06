cdc9a958e0536ff58c1cfaf299db28a2abdc78d6
Shobhit Jain <42944341+Shobhitjain123@users.noreply.github.com>
2025-07-06 16:59:49 +0530

    fix: improve route handling for production environment

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,8 +27,10 @@ app.use("/api/message", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/:path(*)", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get("*", (req, res) => {
+    if (!req.originalUrl.startsWith("/api")) {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    }
   });
 }
 
