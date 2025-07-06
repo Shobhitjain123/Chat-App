287e69f7c113e8ff35105c5b019e6e7fb7fb319a
Shobhit Jain <42944341+Shobhitjain123@users.noreply.github.com>
2025-07-06 16:34:02 +0530

    refactor: streamline server setup and configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,36 +1,38 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import authRoutes from './routes/authRoute.route.js'
-import messageRoutes from './routes/message.route.js'
-import { dbConnect } from './lib/dbConnect.js'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import {app, server} from './lib/socket.js'
-import path from "path"
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoute.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { dbConnect } from "./lib/dbConnect.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
+import path from "path";
 
-dotenv.config()
-const port = process.env.PORT || 8001
-const __dirname = path.resolve()
+dotenv.config();
+const port = process.env.PORT || 8001;
+const __dirname = path.resolve();
 
-app.use(express.json())
-app.use(cors({
+app.use(express.json());
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-}))
-app.use(cookieParser())
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
-app.use('/api/auth', authRoutes)
-app.use('/api/message', messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    })
+  app.get("/:path(*)", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(port, () => {
-    console.log("Listening on Port", port);
-    dbConnect()
-})
+  console.log("Listening on Port", port);
+  dbConnect();
+});
